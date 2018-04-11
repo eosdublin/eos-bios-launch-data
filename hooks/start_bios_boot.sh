572690538106d931d4ebd2d55efb7aa662c831cb
eosdublin <38125983+eosdublin@users.noreply.github.com>
2018-04-11 21:07:10 +0200

    Updated paths

--- hooks/start_bios_boot.sh
+++ hooks/start_bios_boot.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+CONFIGDIR=/etc/eos/scholar
+DATADIR=/var/opt/eos/scholar
+
 # `start_bios_boot` hook
 # $1 genesis JSON
 # $2 ephemeral public key
@@ -9,20 +12,20 @@ echo "Killing running nodes"
 killall nodeos
 
 echo "Phasing out any previous blockchain from disk"
-rm -rf ~/.eos/blocks ~/.eos/shared_mem
+rm -rf $DATADIR/blocks $DATADIR/shared_mem
 
 echo "Copying base config"
 # This one shouldn't contain any `producer-name` nor `private-key` nor `enable-stale-production` statements.
-cp base_config.ini ~/.eos/config.ini
+cp base_config.ini $CONFIGDIR/config.ini
 
 echo "Writing genesis.json"
-echo $1 > ~/.eos/genesis.json
+echo $1 > $CONFIGDIR/genesis.json
 
-echo "plugin = eosio::producer_plugin" >> ~/.eos/config.ini
-echo "producer-name = eosio" >> ~/.eos/config.ini
+echo "plugin = eosio::producer_plugin" >> $CONFIGDIR/config.ini
+echo "producer-name = eosio" >> $CONFIGDIR/config.ini
 
-echo "enable-stale-production = true" >> ~/.eos/config.ini
-echo "private-key = [\"$2\",\"$3\"]" >> ~/.eos/config.ini
+echo "enable-stale-production = true" >> $CONFIGDIR/config.ini
+echo "private-key = [\"$2\",\"$3\"]" >> $CONFIGDIR/config.ini
 
 # Replace this by some automated command to restart the node.
 echo "CONFIGURATION DONE: Re/start nodeos, and press ENTER"
