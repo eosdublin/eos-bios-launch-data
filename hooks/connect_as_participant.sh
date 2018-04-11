9ab009252f29b9d7a9cea7b486bd7c02c840bf00
eosdublin <38125983+eosdublin@users.noreply.github.com>
2018-04-11 21:04:43 +0200

    Updated paths

--- hooks/connect_as_participant.sh
+++ hooks/connect_as_participant.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+CONFIGDIR=/etc/eos/scholar
+DATADIR=/var/opt/eos/scholar
+
 # `connect_as_participant` hook:
 # $1 = p2p_address
 # $2 = public key used by BIOS
@@ -10,21 +13,21 @@ echo "Killing running nodes"
 killall nodeos
 
 echo "Phasing out any previous blockchain from disk"
-rm -rf ~/.eos/blocks ~/.eos/shared_mem
+rm -rf $DATADIR/blocks $DATADIR/shared_mem
 
 echo "Copying base config"
 # This one shouldn't contain any `producer-name` nor `private-key` nor `enable-stale-production` statements.
-cp base_config.ini ~/.eos/config.ini
+cp base_config.ini $CONFIGDIR/config.ini
 
 echo "Writing genesis.json"
-echo $4 > ~/.eos/genesis.json
+echo $4 > $CONFIGDIR/genesis.json
 
 # INSERT YOUR NODE NAME HERE:
-echo "producer-name = eosdublin" >> ~/.eos/config.ini
-echo "plugin = eosio::producer_plugin" >> ~/.eos/config.ini
-echo "p2p-peer-address = $1" >> ~/.eos/config.ini
-echo "private-key = [\"$NODEOS_PRODUCER_PUBLIC_KEY\",\"$NODEOS_PRODUCER_PRIVATE_KEY\"]" >> ~/.eos/config.ini
-echo "resync-blockchain = true" >> ~/.eos/config.ini
+echo "producer-name = eosdublin" >> $CONFIGDIR/config.ini
+echo "plugin = eosio::producer_plugin" >> $CONFIGDIR/config.ini
+echo "p2p-peer-address = $1" >> $CONFIGDIR/config.ini
+echo "private-key = [\"$NODEOS_PRODUCER_PUBLIC_KEY\",\"$NODEOS_PRODUCER_PRIVATE_KEY\"]" >> $CONFIGDIR/config.ini
+echo "resync-blockchain = true" >> $CONFIGDIR/config.ini
 
 # Replace this by some automated command to restart the node.
 echo "CONFIGURATION DONE: Re/start nodeos, and press ENTER"
